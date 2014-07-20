4de40b60c499703c3dd6737d3e40559ecc4ab17d
Hongbin Lu <hongbin@example.com>
2014-07-20 03:12:59 +0000

    Added ability to specify security group for node group templates.

--- saharaclient/api/node_group_templates.py
+++ saharaclient/api/node_group_templates.py
@@ -27,7 +27,7 @@ class NodeGroupTemplateManager(base.ResourceManager):
                       description=None,
                       volumes_per_node=None, volumes_size=None,
                       node_processes=None, node_configs=None,
-                      floating_ip_pool=None):
+                      floating_ip_pool=None, security_groups=None):
 
         data = {
             'name': name,
@@ -40,7 +40,8 @@ class NodeGroupTemplateManager(base.ResourceManager):
         self._copy_if_defined(data,
                               description=description,
                               node_configs=node_configs,
-                              floating_ip_pool=floating_ip_pool)
+                              floating_ip_pool=floating_ip_pool,
+                              security_groups=security_groups)
 
         if volumes_per_node:
             data.update({"volumes_per_node": volumes_per_node,
@@ -50,12 +51,13 @@ class NodeGroupTemplateManager(base.ResourceManager):
 
     def create(self, name, plugin_name, hadoop_version, flavor_id,
                description=None, volumes_per_node=None, volumes_size=None,
-               node_processes=None, node_configs=None, floating_ip_pool=None):
+               node_processes=None, node_configs=None, floating_ip_pool=None,
+               security_groups=None):
 
         data = self._assign_field(name, plugin_name, hadoop_version, flavor_id,
                                   description, volumes_per_node, volumes_size,
                                   node_processes, node_configs,
-                                  floating_ip_pool)
+                                  floating_ip_pool, security_groups)
 
         return self._create('/node-group-templates', data,
                             'node_group_template')
@@ -63,12 +65,14 @@ class NodeGroupTemplateManager(base.ResourceManager):
     def update(self, ng_template_id, name, plugin_name, hadoop_version,
                flavor_id, description=None, volumes_per_node=None,
                volumes_size=None, node_processes=None,
-               node_configs=None, floating_ip_pool=None):
+               node_configs=None, floating_ip_pool=None,
+               security_groups=None):
 
         data = self._assign_field(name, plugin_name, hadoop_version, flavor_id,
                                   description, volumes_per_node,
                                   volumes_size, node_processes,
-                                  node_configs, floating_ip_pool)
+                                  node_configs, floating_ip_pool,
+                                  security_groups)
 
         return self._update('/node-group-templates/%s' % ng_template_id, data,
                             'node_group_template')
